038c7930fcf39501ecf9fbbaf4100c6457f3c49e
吉澤弦気 <genki.yoshizawa.0524@gmail.com>
2021-05-06 14:41:32 +0900

    吉澤弦気：【追加】ブロックのデバッグ情報に配列座標の表示を追加

--- Assets/GenkiYoshizawa/StageEditorDevelop/Scripts/BlockDebug.cs
+++ Assets/GenkiYoshizawa/StageEditorDevelop/Scripts/BlockDebug.cs
@@ -7,10 +7,16 @@ public class BlockDebug : MonoBehaviour
     [Header("ワイヤーカラーの設定")]
     [SerializeField] private Color _WireColor = new Color(1f, 0, 0, 0.5f);
 
+    [Header("ブロックの配列座標(確認用)")]
+    [SerializeField] private Vector2Int _BlockLocalPosition = new Vector2Int();
+    public void Update()
+    {
+        _BlockLocalPosition = transform.GetComponent<BlockConfig>().GetBlockLocalPosition();
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = _WireColor;
         Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
-
 }
