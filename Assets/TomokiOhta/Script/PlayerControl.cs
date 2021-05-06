1ea0937ff6e77514fd7b6c478611ea0dd0a8455f
aka <akapikumin232@gmail.com>
2021-05-06 10:33:47 +0900

    太田朋希 : 【追加】 プレイヤーのパネル操作を追加

--- Assets/TomokiOhta/Script/PlayerControl.cs
+++ Assets/TomokiOhta/Script/PlayerControl.cs
@@ -11,8 +11,6 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]private Vector2Int _Direction;
 
     private Vector2Int _LocalPosition;
-    private GameObject _FrontBlock;
-    private GameObject _BackBlock;
 
     private GameManagerScript _GameManagerScript;
 
@@ -41,6 +39,68 @@ public class PlayerControl : MonoBehaviour
         {
             Debug.Log("上が押されたよ");
             Move(_Direction);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            //Rotateを行うデバッグ用
+
+            //前のブロック取得
+            var block = _GameManagerScript.GetBlock(_LocalPosition + _Direction);
+
+            //前のブロックがRotate可能かどうかを調べる
+            var panelScript = block.transform.GetChild(_IsFront ? 1 : 0).GetComponent<PanelConfig>();
+
+            if (panelScript.GetCanRotate())
+            {
+                var blockControlScript = block.GetComponent<BlockControl>();
+                blockControlScript.Rotate(_IsFront, 90);
+                Debug.Log("Rotateできたよ");
+            }
+            else
+            {
+                Debug.Log("Rotate失敗");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            //Swapを行うデバッグ用
+
+            //前のブロック取得
+            var block = _GameManagerScript.GetBlock(_LocalPosition + _Direction);
+
+            //前のブロックがSwap可能かどうかを調べる
+            var panelScript = block.transform.GetChild(_IsFront ? 1 : 0).GetComponent<PanelConfig>();
+            if (panelScript.GetCanSwap())
+            {
+                var blockControlScript = block.GetComponent<BlockControl>();
+                blockControlScript.Swap(_IsFront);
+            }
+            else
+            {
+                Debug.Log("Swap失敗");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            //TurnOverを行うデバッグ用
+
+            //前のブロック取得
+            var block = _GameManagerScript.GetBlock(_LocalPosition + _Direction);
+
+            //前のブロックがTurnOver可能かどうかを調べる
+            var panelScript = block.transform.GetChild(_IsFront ? 1 : 0).GetComponent<PanelConfig>();
+            if (panelScript.GetCanTurnOver())
+            {
+                var blockControlScript = block.GetComponent<BlockControl>();
+                blockControlScript.TurnOver(_IsFront);
+            }
+            else
+            {
+                Debug.Log("TurnOver失敗");
+            }
         }
 
         //箱コンこれで動くと思う
@@ -72,7 +132,7 @@ public class PlayerControl : MonoBehaviour
         //transform.localPosition = new Vector3(nowpos.x - _MoveDirection.x, nowpos.y - _MoveDirection.y, nowpos.z - _MoveDirection.z);
         //transform.localPosition = nowpos - _MoveDirection;
 
-        //現在位置の取得
+        //前のブロック取得
         var block = _GameManagerScript.GetBlock(_LocalPosition + direction);
 
         //親を切り替える
@@ -85,6 +145,7 @@ public class PlayerControl : MonoBehaviour
 
     public void RotateMySelf(Vector2Int position, float angle)
     {
+        //Rotate時に呼び出される関数、自分の方向を変えるときにも自分で呼ぶ
         if (position != _LocalPosition)
             return;
 
@@ -92,10 +153,31 @@ public class PlayerControl : MonoBehaviour
         direction = Quaternion.Euler(0f, angle, 0f) * direction;
 
         Vector2 tmp = new Vector2(direction.x, direction.z);
-
+        //四捨五入して代入することでVector2Intにも無理やり代入させる
         _Direction = new Vector2Int(Mathf.RoundToInt(tmp.x), Mathf.RoundToInt(tmp.y));
     }
 
+    public void SwapMySelf(Vector2Int position)
+    {
+        //Swap時に呼び出される関数、親オブジェクトであるブロックの移動についていくだけ
+        if (position != _LocalPosition)
+            return;
+
+        transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    public void TurnOverMySelf(Vector2Int position)
+    {
+        //TurnOver時に呼び出される関数、ひっくり返すのはSetIsFrontで良いのでは？
+        if (position != _LocalPosition)
+            return;
+
+        //ひっくり返す
+        if (_IsFront)
+            _IsFront = false;
+        else
+            _IsFront = true;
+    }
 
     public Vector2Int GetLocalPosition() { return _LocalPosition; }
     public bool GetIsFront() { return _IsFront; }
