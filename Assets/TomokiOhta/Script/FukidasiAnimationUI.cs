a500368e6038d8195328a63c447d700b059fdbb5
aka <akapikumin232@gmail.com>
2021-05-20 10:05:38 +0900

    太田朋希 : 【追加修正】 プレイヤーのUIアニメーション作成中

--- Assets/TomokiOhta/Script/FukidasiAnimationUI.cs
+++ Assets/TomokiOhta/Script/FukidasiAnimationUI.cs
@@ -7,7 +7,19 @@ public class FukidasiAnimationUI : MonoBehaviour
     //座標としてプレイヤー追従用 子クラスにしてもよい？
     private GameObject _Player;
 
-    private Animator _Animator;
+    private Animator _FukidasiAnimator;
+
+    [Header("コマンド入れる")]
+    [SerializeField] private List<GameObject> _PanelList;
+    private List<Animator> _PanelAnimList = new List<Animator>();
+
+
+    [Header("選択アイコン入れる")]
+    [SerializeField] private GameObject _Icon;
+    private Animator _IconAnim;
+
+    //どの個数のふきだしを生成するか
+    private int _FukidasiCount = 0;
 
     public void Start()
     {
@@ -15,8 +27,19 @@ public class FukidasiAnimationUI : MonoBehaviour
         var GameManagerScript = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManagerScript>();
         _Player = GameManagerScript.GetPlayer();
 
-        _Animator = GetComponent<Animator>();
+        _FukidasiAnimator = GetComponent<Animator>();
 
+        //パネルの初期化
+        foreach (var panel in _PanelList)
+        {
+            _PanelAnimList.Add(panel.GetComponent<Animator>());
+        }
+        //とりあえず見えなくしておく
+        foreach (var panelAnim in _PanelAnimList)
+        {
+            panelAnim.SetInteger("_ActType", -1);
+        }
+        _IconAnim = _Icon.GetComponent<Animator>();
     }
 
     public void Update()
@@ -29,8 +52,38 @@ public class FukidasiAnimationUI : MonoBehaviour
         //transform.LookAt(p);
     }
 
-    public void SetCount(int num) { _Animator.SetInteger("_ActionCount", num); }
+    public void SetCount(int num)
+    {
+        _FukidasiCount = num;
+        _FukidasiAnimator.SetInteger("_ActionCount", num);
+    }
 
-    public void ResetCount(){ _Animator.SetInteger("_ActionCount", 0); }
+    public void ResetCount()
+    {
+        _FukidasiCount = -1;
+        _FukidasiAnimator.SetInteger("_ActionCount", _FukidasiCount);
+    }
+
+    public int GetCount() { return _FukidasiCount; }
+
+    public void SetPanel(List<int> panelList)
+    {
+        //listの要素をそのまま_PanelAnimListのSetIntしてあげる
+
+        foreach (var panel in panelList)
+        {
+
+        }
+        _IconAnim.SetInteger("_Select", 1);
+    }
+
+    public void ResetPanel()
+    {
+        foreach (var panelAnim in _PanelAnimList)
+        {
+            panelAnim.SetInteger("_ActType", -1);
+        }
+        _IconAnim.SetInteger("_Select", 0);
+    }
 }
 
