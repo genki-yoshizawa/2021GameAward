0eb42c3f5f6a7281f310f996909246477242c29d
吉澤弦気 <genki.yoshizawa.0524@gmail.com>
2021-07-09 02:45:32 +0900

    吉澤弦気：【追加】新規ゲームシーンUIをプレハブ化した

--- Assets/HiromuKaneko/Script/EnemyControl.cs
+++ Assets/HiromuKaneko/Script/EnemyControl.cs
@@ -74,11 +74,6 @@ public class EnemyControl : MonoBehaviour
         _CheeseBite = false;
         _IsExist = false;
         _PassedTime = 0.0f;
-<<<<<<< HEAD
-=======
-        // マネージャーのスタートアニメーションを呼ぶ
-        //        _GameManager.gameObject.GetComponent<GameManagerScript>().StartEnemyMovie();
->>>>>>> d8a11013a63d749209499476537f8bc5567ba6c8
 
 
     }
@@ -943,12 +938,6 @@ public class EnemyControl : MonoBehaviour
 
         else
             _IsFront = true;
-<<<<<<< HEAD
-=======
-
-        //        _GameManager.gameObject.GetComponent<GameManagerScript>().StartEnemyMovie();  
-
->>>>>>> d8a11013a63d749209499476537f8bc5567ba6c8
     }
 
     // ブロック側で呼び出す　自分の位置を入れ替える関数
